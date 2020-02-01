890039eaadb322719b95b674a522862be3c48c8c
karolis <karolissamuolis@gmail.com>
2020-02-01 23:55:13 +0200

    added pictures

---
+++ precache-manifest.d3db83487c757f656a1ce2f8cb8f10e9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9da4349695004cb25f6b73a2727584e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b9786bea6fb03b7780e",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "5e0783bc1d69d2c61de7",
+    "url": "/static/css/main.1031f96a.chunk.css"
+  },
+  {
+    "revision": "0b9786bea6fb03b7780e",
+    "url": "/static/js/2.8e7e9c21.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.8e7e9c21.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5e0783bc1d69d2c61de7",
+    "url": "/static/js/main.8b97e2b2.chunk.js"
+  },
+  {
+    "revision": "73a8c5debd7b776517f1",
+    "url": "/static/js/runtime-main.d703c616.js"
+  },
+  {
+    "revision": "464a6055f319d4aab7fa23410d4c6a8b",
+    "url": "/static/media/doc.464a6055.png"
+  },
+  {
+    "revision": "2c9eff4c5fc6111188867acec52d2950",
+    "url": "/static/media/folder3.2c9eff4c.png"
+  },
+  {
+    "revision": "cd354081a54ceeb084d35ec488edc46b",
+    "url": "/static/media/github2.cd354081.png"
+  },
+  {
+    "revision": "e25086b266a080f644c9543f3b385ab0",
+    "url": "/static/media/info.e25086b2.png"
+  },
+  {
+    "revision": "769489646018e2759a8f9b5ca826e68f",
+    "url": "/static/media/linkedin7.76948964.png"
+  },
+  {
+    "revision": "651b9d5efba9f036b2cbd90e545170c6",
+    "url": "/static/media/mailred.651b9d5e.png"
+  }
+]);
