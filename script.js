937b6e6e3b308de0a414776c1a7f630ec9548617
Karolis Samuolis <karolissamuolis@gmail.com>
2021-01-31 17:34:18 +0200

    Style changes

---
+++ script.js
@@ -0,0 +1,9 @@
+let toggledMenu = false;
+
+const openMenu = () => {
+    if(toggledMenu)
+        document.getElementById("hamburger-items").style.right = "-500px";
+    else
+        document.getElementById("hamburger-items").style.right = "calc(2vw - 3px)";
+    toggledMenu = !toggledMenu;
+}
