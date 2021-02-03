5cf75eaa1304667aa9d5ebc2baef04775d88298b
Karolis Samuolis <karolissamuolis@gmail.com>
2021-02-03 21:44:21 +0200

    Changed to single-page app

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 let toggledMenu = false;
+var fr = new FileReader();
+
+document.onreadystatechange = function () {
+    if (document.readyState == "complete") {
+        function loadContent() {
+            var fragmentId = location.hash.substr(1);
+            fetch(`./pages/${fragmentId}.html`)
+                .then((response) => response.text())
+                .then((data) => {
+                        document.getElementById("app").innerHTML = data;
+                    });
+    
+        }
+
+        if (!location.hash) {
+            location.hash = "#home";
+        }
+        loadContent();
+
+        window.addEventListener("hashchange", loadContent);
+    }
+};
 
 const openMenu = () => {
-    if(toggledMenu)
+    if (toggledMenu)
         document.getElementById("hamburger-items").style.right = "-500px";
     else
-        document.getElementById("hamburger-items").style.right = "calc(2vw - 3px)";
+        document.getElementById("hamburger-items").style.right =
+            "calc(2vw - 3px)";
     toggledMenu = !toggledMenu;
-}
+};
 
-console.log(window.location.hostname.split("."));
+
+
