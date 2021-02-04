8279f170a5c34703c2b6ad4b549c8128db333b91
Karolis Samuolis <karolissamuolis@gmail.com>
2021-02-04 14:27:42 +0200

    Added contact form

--- script.js
+++ script.js
@@ -10,15 +10,16 @@ document.onreadystatechange = function () {
                 .then((data) => {
                     document.getElementById("app").innerHTML = data;
                     assignActive(fragmentId);
+                    if (fragmentId == "contact") {
+                        setForm();
+                    }
                 });
-    
-        }
+        };
 
         if (!location.hash) {
             location.hash = "#home";
         }
         loadContent();
-
         window.addEventListener("hashchange", loadContent);
     }
 };
@@ -28,11 +29,11 @@ const assignActive = (fragmentId) => {
     [].forEach.call(activeElements, function (el) {
         el.classList.remove("active");
     });
-    var toActivateElements = document.querySelectorAll("."+fragmentId);
+    var toActivateElements = document.querySelectorAll("." + fragmentId);
     [].forEach.call(toActivateElements, function (el) {
         el.classList.add("active");
     });
-}
+};
 
 const openMenu = () => {
     if (toggledMenu)
@@ -43,4 +44,53 @@ const openMenu = () => {
     toggledMenu = !toggledMenu;
 };
 
+window.onclick = (event) => {
+    if (
+        event.target != document.getElementById("hamburger") &&
+        event.target != document.getElementById("hamburger-items") &&
+        event.target != document.getElementById("work") &&
+        event.target != document.getElementById("about") &&
+        event.target != document.getElementById("contact")
+    ) {
+        document.getElementById("hamburger-items").style.right = "-500px";
+        toggledMenu = !toggledMenu;
+    }
+};
 
+const setForm = () => {
+    var form = document.getElementById("contact-form");
+    document.getElementById("submit").onclick = function () {
+        form.addEventListener("submit", function (ev) {
+            ev.preventDefault();
+            var data = new FormData(form);
+            ajax(`POST`, `https://formspree.io/f/mbjplqle`, data);
+        });
+        function success() {
+            form.reset();
+            document.getElementById(
+                "submit"
+            ).innerHTML = `<i class="fas fa-check"></i>`;
+            document.getElementById("submit").style.backgroundColor = "#61cc40";
+            setTimeout(function () {
+                document.getElementById(
+                    "submit"
+                ).innerHTML = `<i class="fas fa-paper-plane"></i>`;
+                document.getElementById("submit").style.backgroundColor = "var(--accent)";
+            }, 3000);
+        }
+        function ajax(method, url, data) {
+            var xhr = new XMLHttpRequest();
+            xhr.open(method, url);
+            xhr.setRequestHeader("Accept", "application/json");
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState !== XMLHttpRequest.DONE) return;
+                if (xhr.status === 200) {
+                    success(xhr.response, xhr.responseType);
+                } else {
+                    error(xhr.status, xhr.response, xhr.responseType);
+                }
+            };
+            xhr.send(data);
+        }
+    };
+};
